c20a352a3943f76ec1d98f32af18e3a920c17281
Gaetano Perrone <gaetanoperrone@MBP-di-Gaetano.fritz.box>
2017-04-22 09:50:13 +0200

    Test api started

---
+++ test/api_test.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var chai = require('chai');
+ , chaiHttp = require('chai-http');
+chai.expect();
+chai.should();
+
+
+describe('api test' () => {
+
+
+var urlBase 
+//Before testing choice and empty directory and set these vars 
+var cloneExample 
+var cloneWithAuth 
+
+
+      before(() => {} 
+
+	urlBase = "http://localhost:8080/repogit/v1/" 
+	//Before testing choice and empty directory and set these vars 
+	cloneExample = "https://github.com/giper45/rup_templates.git" 
+	cloneWithAuth = "" 
+     )
+     //Correct conditions 
+     //Clone
+    it('should clone a new repo', function () {
+		
+
+    });
+
+
+	
+
+
+    //Clone with auth
+    it('should clone with auth, () => {
+
+    //Update with auth 
+
+    //Delete 
+    
+
+    //Get repos 
+
+
+
+
+   // 
+
+
+
+
+    //Wrong conditions 
+
+
+    //Clone and rootDir not setted 
+
+
+
+    it('should give an error if I'm not authorized to clone the repo', () => { 
+
+
+    });
+   it("shol
+
+
+
+
+});
+
